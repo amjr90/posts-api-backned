c597f06472e97027bd889bce62ce500a88cbaebe
andres jaramillo <andresmjaramillo@yape.com.pe>
2025-12-16 11:25:52 -0500

    update

--- index.js
+++ index.js
@@ -68,7 +68,8 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Endpoint not found' })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
