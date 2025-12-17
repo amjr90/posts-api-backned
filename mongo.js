aec304960c217dcc3722f3a46e29c0ab054cd520
andres jaramillo <andresmjaramillo@yape.com.pe>
2025-12-17 10:08:23 -0500

    feat: add uncaught exception handling for MongoDB connection

--- mongo.js
+++ mongo.js
@@ -9,3 +9,16 @@ mongoose.connect(connectionString)
   .catch(err => {
     console.error('Error connecting to MongoDB:', err)
   })
+
+process.on('uncaughtException', err => {
+  console.error('Uncaught Exception:', err)
+  mongoose.connection.disconnect()
+    .then(() => {
+      console.log('Disconnected from MongoDB due to uncaught exception')
+      process.exit(1)
+    })
+    .catch(disconnectErr => {
+      console.error('Error disconnecting from MongoDB:', disconnectErr)
+      process.exit(1)
+    })
+})
